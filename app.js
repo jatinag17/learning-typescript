c3c058a5762e1d8dcb108d8bedc2f80e8f28616d
jatinag17 <jatinagrawal128@gmail.com>
2024-12-30 13:42:00 +0530

    understanding type allias

--- app.js
+++
@@ -1,7 +0,0 @@
-// 
-function getTotal(numbers) {
-    return numbers.reduce(function (acc, item) {
-        return acc + item;
-    }, 0); // initial value of acc. is equal to 0
-}
-console.log(getTotal([3, 2, 1])); // Output: 6
--- app.ts
+++ app.ts
@@ -6,11 +6,51 @@
 
 
 
-function getTotal(numbers:Array<number>) {  // this is generic which is seems only in development mode
-    return numbers.reduce((acc,item)=>{
-        return acc + item;
-    },0); // initial value of acc. is equal to 0
-}
+// function getTotal(numbers:Array<number>) {  // this is generic which is seems only in development mode
+//     return numbers.reduce((acc,item)=>{
+//         return acc + item;
+//     },0); // initial value of acc. is equal to 0
+// }
 
-console.log(getTotal([3,2,1])); // Output: 6
+// console.log(getTotal([3,2,1])); // Output: 6
 
+
+
+// const user ={
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     role:'professor',
+// };
+// console.log(user.firstName); // Output: "John
+
+
+// number 
+// string
+//array[number, string]  array can be number or string
+//boolean
+
+//type alias
+
+// type User={
+//     name: string;
+//     age: number;
+//     address?: string;
+// }
+
+
+
+// const user: User={
+//   name: "John",
+//   age: 30,
+// };
+
+
+// function login(userData:User):User {
+//   return userData;
+// }
+// console.log(login({name: "John", age:30})); 
+
+
+type ID=number | string;
+
+const userId:ID='123'; 
